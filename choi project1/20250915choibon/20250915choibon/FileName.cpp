d039b1f70a6b75dfec1b0dee6331b55cb2235016
MrCBon <choibon13012@gmail.com>
2025-09-15 16:18:02 +0900

    20250915 project 1

---
+++ choi project1/20250915choibon/20250915choibon/FileName.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("=== Basic Constants ===\n");
+	printf("Name\t: sungwook\n");
+	printf("Initial\t: S \n");
+	printf("Age : %d\n", 28);
+	printf("Score\t: %lf\n", 93.5);
+	printf("Count\t: 3000000000\n");
+	return 0;
+}
